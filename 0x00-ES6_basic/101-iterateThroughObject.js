e01418ac96dd251952066c4df140a7762678382f
Mostafa-sandbox <mostafa.jamal.mjm@gmail.com>
2024-04-24 00:36:48 +0000

    refactor: to a single line function

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,10 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let employessString = '';
-  for (const employee of reportWithIterator) {
-    if (reportWithIterator.indexOf(employee) > 0) {
-      employessString += ' | ';
-    }
-    employessString += employee;
-  }
-  return employessString;
+  return reportWithIterator.join(' | ');
 }
