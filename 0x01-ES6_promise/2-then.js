ef6d55728df83df8108fc667c847aef69e777af4
Mostafa-sandbox <mostafa.jamal.mjm@gmail.com>
2024-04-24 18:07:23 +0000

    fix: red output check try1

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then((result) => result)
-    .catch(() => Error())
+    .then(success => {
+      return {status: 200, body: success};
+    })
+    .catch((e) => Error())
     .finally(() => {
       console.log('Got a response from the API');
     });
