583401188f4c16a0fca2b885d9221b29e1cce2ef
Mostafa-sandbox <mostafa.jamal.mjm@gmail.com>
2024-04-24 22:33:04 +0000

    refactor and fix: minor changes

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then(success => {
-      return {status: 200, body: success};
-    })
-    .catch((e) => Error())
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
