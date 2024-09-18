1582ccbb98d28579e02e3cc9e012b01528d35e3b
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-19 02:36:12 +0300

    5. Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,1 +1,59 @@
-#!/usr/bin/node
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+async function countStudents(localPath) {
+  const fullPath = path.resolve(localPath);
+  let output = 'This is the list of our students\n';
+  try {
+    const data = await fs.promises.readFile(fullPath, 'utf8');
+    const lines = data.split('\n');
+    const students = lines.filter((line) => line.trim() !== '');
+    students.shift();
+    output += `Number of students: ${students.length}\n`;
+    const studentsList = new Map();
+    for (const student of students) {
+      const [firstName, , , field] = student.split(',');
+      if (!studentsList.has(field)) {
+        studentsList.set(field, [firstName]);
+      } else {
+        studentsList.get(field).push(firstName);
+        studentsList.set(field, studentsList.get(field));
+      }
+    }
+    for (const [key, value] of studentsList) {
+      output += `Number of students in ${key}: ${value.length}. List: ${value.join(', ')}\n`;
+    }
+    output = output.trimEnd();
+    return output;
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+const filePath = process.argv[2];
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  switch (req.url) {
+    case '/':
+      res.end('Hello Holberton School!');
+      break;
+    case '/students':
+      countStudents(filePath)
+        .then((data) => {
+          res.end(data);
+        })
+        .catch((err) => {
+          res.end(err.message);
+        });
+      break;
+    default:
+      res.end('Hello Holberton School!');
+      break;
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
