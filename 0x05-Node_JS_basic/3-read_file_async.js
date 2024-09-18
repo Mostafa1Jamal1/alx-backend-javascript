429c1452dc8844c4b40a982a20f9ca8f3811cac3
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-18 23:25:03 +0300

    3. Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,1 +1,32 @@
-#!/usr/bin/node
+const fs = require('fs');
+const path = require('path');
+
+async function countStudents(localPath) {
+  const fullPath = path.resolve(localPath);
+  try {
+    const data = await fs.promises.readFile(fullPath, 'utf8');
+    const lines = data.split('\n');
+    const students = lines.filter((line) => line.trim() !== '');
+    students.shift();
+    console.log(`Number of students: ${students.length}`);
+    const studentsList = new Map();
+    for (const student of students) {
+      const [firstName, , , field] = student.split(',');
+      if (!studentsList.has(field)) {
+        studentsList.set(field, [firstName]);
+      } else {
+        studentsList.get(field).push(firstName);
+        studentsList.set(field, studentsList.get(field));
+      }
+    }
+    for (const [key, value] of studentsList) {
+      console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+    }
+    return data;
+  } catch (err) {
+    console.log(err);
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
