095d32362d92252702cc745b67e5e71714b1be03
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-18 23:06:10 +0300

    2. Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,1 +1,30 @@
-#!/usr/bin/node
+const fs = require('node:fs');
+const path = require('node:path');
+
+function countStudents(localPath) {
+  const fullPath = path.resolve(localPath);
+  try {
+    const data = fs.readFileSync(fullPath, 'utf-8');
+    const lines = data.split('\n');
+    const students = lines.filter((line) => line.trim() !== '');
+    students.shift();
+    console.log(`Number of students: ${students.length}`);
+    const studentsList = new Map();
+    for (const student of students) {
+      const [firstName, , , field] = student.split(',');
+      if (!studentsList.has(field)) {
+        studentsList.set(field, [firstName]);
+      } else {
+        studentsList.get(field).push(firstName);
+        studentsList.set(field, studentsList.get(field));
+      }
+    }
+    for (const [key, value] of studentsList) {
+      console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
