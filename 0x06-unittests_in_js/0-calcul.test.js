4bee64f5b1f23e86afbe3232282fb0a90eac6ddf
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-20 21:20:29 +0300

    test: there is missing test case

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,19 +2,22 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('should return the sum of 1 and 3 equal 4', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-  it('should return the sum of 1 and 3.7 equal 5', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
-  it('should return the sum of 1.2 and 3.7 equal 5', () => {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
-  it('should return the sum of 1.5 and 3.7 equal 6', () => {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
-  it('should return the sum of 1.4999999 and 3.7 equal 5', () => {
-    assert.strictEqual(calculateNumber(1.4999999, 3.7), 5);
-  });
+    it('should return the sum of 1 and 3 equal 4', () => {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+    it('should return the sum of 1 and 3.7 equal 5', () => {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+    it('should return the sum of 1 and 3.4 equal 4', () => {
+        assert.strictEqual(calculateNumber(1, 3.4), 4);
+    });
+    it('should return the sum of 1.2 and 3 equal 4', () => {
+        assert.strictEqual(calculateNumber(1.2, 3), 4);
+    });
+    it('should return the sum of 1.5 and 3 equal 5', () => {
+        assert.strictEqual(calculateNumber(1.5, 3), 5);
+    });
+    it('should return the sum of 1.4999999 and 3.4999999 equal 4', () => {
+        assert.strictEqual(calculateNumber(1.4999999, 3), 4);
+    });
 });
