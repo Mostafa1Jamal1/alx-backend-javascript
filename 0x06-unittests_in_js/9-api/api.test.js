3172a4399237e2037e1101e2fcd07e6320b821ea
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-21 02:51:53 +0300

    9. Regex integration testing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,1 +1,35 @@
-#!/usr/bin/node
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Index page', () => {
+    it('should return 200', (done) => {
+        request('http://localhost:7865', (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('should return "Welcome to the payment system"', (done) => {
+        request('http://localhost:7865', (error, response, body) => {
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
+})
+
+describe('Cart page', () => {
+    it('should return 404 when id is not a number', (done) => {
+        request('http://localhost:7865/cart/notANumber', (error, response, body) => {
+            expect(response.statusCode).to.equal(404);
+            done();
+        });
+    });
+
+    it('should return 200 when id is a number', (done) => {
+        request('http://localhost:7865/cart/123', (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+})
