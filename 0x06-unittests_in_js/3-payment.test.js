552ba35286312f1c363d222b2b29a78c45f450f8
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-21 00:30:52 +0300

    3. Spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment.js');
+const Utils = require('./utils.js');
+console.log(sendPaymentRequestToApi);
+describe('sendPaymentRequestToApi', () => {
+    it('should call calculateNumber with type SUM', () => {
+        const spy = sinon.spy(Utils, 'calculateNumber');
+        sendPaymentRequestToApi(100, 20);
+        expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+        spy.restore();
+    });
+});
