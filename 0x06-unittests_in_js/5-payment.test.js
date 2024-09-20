203ccd35a56eeef1a8e65f0e359d9f0ae9820a57
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-21 01:25:25 +0300

    5. Hooks

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,1 +1,31 @@
-#!/usr/bin/node
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment.js');
+const Utils = require('./utils.js');
+
+describe('sendPaymentRequestToApi', () => {
+    beforeEach(() => {
+        spy = sinon.spy(console, 'log');
+    })
+
+    afterEach(() => {
+        sinon.restore();
+    })
+
+    it('should call sendPaymentRequestToAPI with 100, and 20', () => {
+        sendPaymentRequestToApi(100, 20);
+
+        expect(spy.calledWith('The total is: 120')).to.be.true;
+        expect(spy.calledOnce).to.be.true;
+        spy.restore();
+    });
+
+    it('should call sendPaymentRequestToAPI with 10, and 10', () => {
+        sendPaymentRequestToApi(10, 10);
+
+        expect(spy.calledWith('The total is: 20')).to.be.true;
+        expect(spy.calledOnce).to.be.true;
+        spy.restore();
+    });
+});
