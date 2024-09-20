bf8b7be6cfcf1025bd21c80ab456d20c5bacac56
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-20 21:02:51 +0300

    fix: test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,15 +3,15 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('should return the sum of 1 and 3 equal 4', () => {
-    assert.strictEqual(calculateNumber(1, 2), 3);
+    assert.strictEqual(calculateNumber(1, 3), 3);
   });
   it('should return the sum of 1 and 3.7 equal 5', () => {
-    assert.strictEqual(calculateNumber(1, 2), 3);
+    assert.strictEqual(calculateNumber(1, 3.7), 3);
   });
   it('should return the sum of 1.2 and 3.7 equal 5', () => {
-    assert.strictEqual(calculateNumber(1, 2), 3);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 3);
   });
   it('should return the sum of 1.5 and 3.7 equal 6', () => {
-    assert.strictEqual(calculateNumber(1, 2), 3);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 3);
   });
 });
