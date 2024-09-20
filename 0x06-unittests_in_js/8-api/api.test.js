045db5ccbbba3e6f7c9e84bb52e23182d2b5e903
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-21 02:39:21 +0300

    8. Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Index page', () => {
+    it('should return 200', (done) => {
+        request('http://localhost:7865', (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('should return "Welcome to the payment system"', (done) => {
+        request('http://localhost:7865', (error, response, body) => {
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
+})
