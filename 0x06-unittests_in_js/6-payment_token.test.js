73528400720e8685d99ecc5df4279725d36d241b
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-21 02:24:52 +0300

    6. Async tests with done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('getPaymentTokenFromAPI', () => {
+    it('should call getPaymentTokenFromAPI', (done) => {
+        getPaymentTokenFromAPI(true).then((res) => {
+            expect(res.data).to.be.equal('Successful response from the API');
+            done();
+        });
+    });
+})
