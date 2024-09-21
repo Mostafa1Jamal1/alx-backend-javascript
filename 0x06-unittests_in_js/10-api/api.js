6b965c0d74f78eafb9cd7b4ed71f6b4870fd2923
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-21 03:59:06 +0300

    10. Deep equality & Post integration testing

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,1 +1,37 @@
-#!/usr/bin/node
+const express = require('express');
+
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', (req, res) => {
+    const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+        res.status(404).send('Product id should be a number');
+    } else {
+        res.send(`Payment methods for cart ${id}`);
+    }
+});
+
+app.get('/available_payments', (req, res) => {
+    res.send({
+        payment_methods: {
+            credit_cards: true,
+            paypal: false
+        }
+    });
+});
+
+app.use(express.json());
+app.post('/login', (req, res) => {
+    res.send(`Welcome ${req.body.userName}`);
+});
+
+app.listen(port);
+
+console.log(`API available on localhost port ${port}`);
+module.exports = app;
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,1 +1,80 @@
-#!/usr/bin/node
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Index page', () => {
+    it('should return 200', (done) => {
+        request('http://localhost:7865', (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('should return "Welcome to the payment system"', (done) => {
+        request('http://localhost:7865', (error, response, body) => {
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
+})
+
+describe('Cart page', () => {
+    it('should return 404 when id is not a number', (done) => {
+        request('http://localhost:7865/cart/notANumber', (error, response, body) => {
+            expect(response.statusCode).to.equal(404);
+            done();
+        });
+    });
+
+    it('should return 200 when id is a number', (done) => {
+        request('http://localhost:7865/cart/123', (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('Payment methods for cart 123');
+            done();
+        });
+    });
+
+    it('should return 200 when id is a float', (done) => {
+        request('http://localhost:7865/cart/1.23', (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('should return 404 when no id', (done) => {
+        request('http://localhost:7865/cart/', (error, response, body) => {
+            expect(response.statusCode).to.equal(404);
+            done();
+        });
+    });
+})
+
+describe('Available payments page', () => {
+    it('should return 200 and correct body', (done) => {
+        request('http://localhost:7865/available_payments', (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            expect(response.body).to.equal('{"payment_methods":{"credit_cards":true,"paypal":false}}');
+            done();
+        });
+    });
+})
+
+describe('Login page', () => {
+    it('should return 200 and correct body', (done) => {
+        const options = {
+            method: 'POST',
+            url: 'http://localhost:7865/login',
+            Headers: {
+                'Content-Type': 'application/json'
+            },
+            json: {
+                userName: 'user',
+            }
+        }
+        request(options, (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('Welcome user');
+            done();
+        });
+    });
+})
